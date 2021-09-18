0edabd6ffa377c05680b02dead40a066a8405fee
Mrs.Dukes <mrs.knit.wit.dukes@gmail.com>
2021-09-18 10:13:08 -0700

    addition of project package and creation of server file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+let PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/public", express.static(path.join(__dirname, "public"));
+
+require("./routes/api")(app);
+require("./routes/html")(app);
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT" + PORT);
+    console.log("http://localhost:" + PORT);
+});
