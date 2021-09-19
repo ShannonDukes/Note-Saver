b9f6fd911056e8b7242188ba9212a4a9802f07ff
Mrs.Dukes <mrs.knit.wit.dukes@gmail.com>
2021-09-18 18:24:04 -0700

    trying different port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-let PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
